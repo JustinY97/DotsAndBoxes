445eea310e57335863668b230d4b4209a5b57818
Justin <69814530+JustinY97@users.noreply.github.com>
2022-03-01 16:22:27 -0500

    Initial Commit

---
+++ app/src/main/java/com/justin/dotsandboxes/GameBoard.java
@@ -0,0 +1,41 @@
+package com.justin.dotsandboxes;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class GameBoard extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_board);
+        LinearLayout layout = new LinearLayout(this);
+        TableLayout game_board = new TableLayout(this);
+        int rows = 10;
+        int columns = 10;
+        for(int i = 0; i < rows; i++){
+            LinearLayout tableRow = new LinearLayout(this);
+            for(int j = 0; j < columns; j++){
+                ImageView node = new ImageView(this);
+                node.setImageResource(R.drawable.board_node);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(50, 50);
+                lp.setMargins(20, 20, 20, 20);
+
+                node.setLayoutParams(lp);
+                tableRow.addView(node);
+
+            }
+            game_board.addView(tableRow);
+        }
+        layout.addView(game_board);
+        setContentView(layout);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/justin/dotsandboxes/MainActivity.java
@@ -0,0 +1,26 @@
+package com.justin.dotsandboxes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startBtn = findViewById(R.id.startButton);
+
+        startBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, GameBoard.class);
+            startActivity(intent);
+        });
+
+
+    }
+}
